4511382f6e9b37231d6ac303e4c20c037aa42d79
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-08-12 16:34:28 +0800

    多次序列化的场景

--- growth-serializable/src/main/java/com/zhou/serializable/SerializableDemo.java
+++ growth-serializable/src/main/java/com/zhou/serializable/SerializableDemo.java
@@ -17,20 +17,52 @@ public class SerializableDemo {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         serializable();
-        deSerializable();
+//        deSerializable();
     }
 
     //序列化操作
-    public static void serializable() throws IOException {
+    public static void serializable() throws IOException, ClassNotFoundException {
 
         Person person = new Person();
 //        person.setAge(18);
         person.setName("周");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(new File("person")));
         objectOutputStream.writeObject(person);
+        objectOutputStream.flush();
+
+        objectOutputStream.writeObject(person);
+        objectOutputStream.flush();
+
+        Person person1 = new Person();
+        person1.setName("new name ");
+        person1.setAddress("new Address");
+        person1.setHeight(123);
+        objectOutputStream.writeObject(person1);
+        objectOutputStream.flush();
+
         objectOutputStream.close();
 
+        System.out.println(new File("person").length());
+
         System.out.println("序列化成功");
+
+        System.out.println("开始反序列化");
+        //第一次
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(new File("person")));
+        Person dePerson1 = (Person) objectInputStream.readObject();
+        System.out.println("第一序列化：" + dePerson1);
+        Person dePerson2 = (Person) objectInputStream.readObject();
+        System.out.println("第一序列化：" + dePerson2);
+        Person dePerson3 = (Person) objectInputStream.readObject();
+        System.out.println("第一序列化：" + dePerson3);
+
+        /**
+         * 如果多次序列化  将会导致异常EOFException
+         */
+        /*Person dePerson4 = (Person) objectInputStream.readObject();
+        System.out.println("第一序列化：" + dePerson4);*/
+
+        objectInputStream.close();
     }
 
     //反序列化操作
