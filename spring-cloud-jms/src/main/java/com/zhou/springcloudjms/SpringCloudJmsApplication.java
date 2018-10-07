90acc27eb01013149346e4311f6547fbe0900a61
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-10-07 10:22:27 +0800

    增加jms模块

---
+++ spring-cloud-jms/src/main/java/com/zhou/springcloudjms/SpringCloudJmsApplication.java
@@ -0,0 +1,12 @@
+package com.zhou.springcloudjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudJmsApplication.class, args);
+    }
+}
