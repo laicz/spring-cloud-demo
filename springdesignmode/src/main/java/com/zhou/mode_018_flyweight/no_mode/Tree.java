f7439ca91e4ae88ec9990fe0ae5e8d8e23103377
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-11-01 22:52:15 +0800

    设置模式 之 享元模式

---
+++ springdesignmode/src/main/java/com/zhou/mode_018_flyweight/no_mode/Tree.java
@@ -0,0 +1,19 @@
+/**
+ * Date:     2018/11/122:42
+ * AUTHOR:   Administrator
+ */
+package com.zhou.mode_018_flyweight.no_mode;
+
+/**
+ * 2018/11/1  22:42
+ * created by zhoumb
+ */
+public class Tree {
+    private int xCoord,yCoord,age;
+
+    public Tree(int xCoord, int yCoord, int age) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.age = age;
+    }
+}
