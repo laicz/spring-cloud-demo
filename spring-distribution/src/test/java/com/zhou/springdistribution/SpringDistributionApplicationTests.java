a72c41a9ec05d8c18ebf52561eda948f348d0fb1
zhouzhuang2017 <zhouzhuang2017@163.com>
2018-11-07 21:58:22 +0800

    增加数据结构和算法项目

---
+++ spring-distribution/src/test/java/com/zhou/springdistribution/SpringDistributionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhou.springdistribution;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDistributionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
